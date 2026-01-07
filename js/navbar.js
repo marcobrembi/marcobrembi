33719aeb21642b238be63b2941eac1ed197d2b6d
marcobrembi <marco.brembi@gmail.com>
2026-01-07 21:56:33 +0100

    ewd

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("elements/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        })
-        .catch(error => console.error("Errore nel caricamento della navbar:", error));
-});
--- js/side.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("elements/side.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("side").innerHTML = data;
-        })
-        .catch(error => console.error("Errore nel caricamento della sidebar:", error));
-});
