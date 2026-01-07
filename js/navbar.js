af8de117ded7a0a24139a948f95954d921dc77a2
marcobrembi <marco.brembi@gmail.com>
2026-01-07 21:47:42 +0100

    f

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("../elements/navbar.html")
+    fetch("elements/navbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
--- js/side.js
+++ js/side.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("../elements/side.html")
+    fetch("elements/side.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("side").innerHTML = data;
